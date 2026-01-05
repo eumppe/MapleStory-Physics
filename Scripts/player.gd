c3e3b36c538f2f91e7214e7e653c5829941e133f
eumppe <pych05@unist.ac.kr>
2026-01-06 06:44:08 +0900

    TODO: 마찰력, 속도, 움직이는 힘 등 구분

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
-const SPEED = 200.0
+#TODO
+const SPEED = 300.0
+const FRICTION_POW = 200.0
 const MOVE_ACCEL = 1600
+
 const JUMP_VELOCITY = -700.0
 const GRAVITY = 2100
 
@@ -20,11 +22,26 @@ func get_friction() -> float:
 func clamp_accel(linear_velocity:float, accel:float, max_velocity:float) -> float:
 	return clamp(linear_velocity+accel,-max_velocity,max_velocity) - linear_velocity
 
+var face = 1
+var djc = 0
+var maxdj = 2
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("key_left", "key_right")
+	if direction:
+			face = direction
+	
+	if direction:
+			velocity.x += clamp_accel(velocity.x, direction * MOVE_ACCEL * get_friction() * delta, SPEED) 
+	else:
+		if abs(velocity.x)>0:
+			var ff = sign(velocity.x) * MOVE_ACCEL * get_friction() * delta
+			velocity.x -= ff
+			if abs(velocity.x) < ff:
+				velocity.x = 0
+	
 	# Add the gravity.
 	if is_on_floor():
-		
+		djc=0
 		# Handle jump.
 		if Input.is_action_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
@@ -35,23 +52,21 @@ func _physics_process(delta: float) -> void:
 		# Default Friction
 		
 		if abs(velocity.x)>SPEED:
-			velocity.x -= clamp((velocity.x-SPEED*sign(velocity.x)) * get_friction(),-SPEED,SPEED)*delta*60
+			velocity.x -= clamp((velocity.x-FRICTION_POW*sign(velocity.x)) * get_friction(),-SPEED,SPEED)*delta*60
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		if direction:
-			velocity.x += clamp_accel(velocity.x, direction * MOVE_ACCEL * get_friction() * delta, SPEED) 
-		else:
-			if abs(velocity.x)>0:
-				var ff = sign(velocity.x) * MOVE_ACCEL * get_friction() * delta
-				velocity.x -= ff
-				if abs(velocity.x) < ff:
-					velocity.x = 0
+		
 	
 	else:
-		if Input.is_action_just_pressed("jump"):
+		var dj_speed=SPEED+300
+		var dj_acc=SPEED+300
+		var dj_up_speed=SPEED+300
+		var dj_up_acc=SPEED+300
+		if djc<maxdj and Input.is_action_just_pressed("jump"):
+			djc+=1
 			if abs(velocity.x) < SPEED + 500:
-				velocity.x += clamp_accel(velocity.x, 500*direction, SPEED+500)
+				velocity.x += clamp_accel(velocity.x, dj_acc*face, dj_speed)
 			if abs(velocity.y) < SPEED + 200:
 				velocity.y -= 200
 		velocity += GRAVITY * delta * Vector2.DOWN
